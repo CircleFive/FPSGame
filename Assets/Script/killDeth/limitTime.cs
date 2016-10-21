0b6802910bdbba7cf35517b83cda96ffdb708b52
YAGISITAJINYA <iwgm14yjinya@gmail.com>
2016-10-21 23:04:55 +0900

    ゲームシーン関係
    limitTimeを入れ忘れました。

--- Assets/Script/killDeth/limitTime.cs
+++ Assets/Script/killDeth/limitTime.cs
@@ -5,33 +5,52 @@ using UnityEngine.SceneManagement;
 
 public class limitTime : MonoBehaviour {
 
-    float playTime = 180;
     [SerializeField]
-    private Text timeText;
+    private float playTime = 180;
+    [SerializeField]
+    private Text timeText1;
+    [SerializeField]
+    private Text timeText2;
     private GameObject C;
-    [SerializeField]
-    private GameObject canvas;
-    [SerializeField]
-    private GameObject camera;
-    [SerializeField]
-    private GameObject panel;
+    //[SerializeField]
+    //private GameObject canvas;
+    //[SerializeField]
+    //private GameObject camera;
+    //[SerializeField]
+    //private GameObject panel;
 
     public string V_O_D; // Victory or defeat
 
-    objectHp p;
+    ObjDestroy p;
     public string pp;
+    public string p1Kill;
+    public string p2Kill;
+    public string p1Point;
+    public string p2Point;
+    public string p1Total;
+    public string p2Total;
+    public string textColor;
+
+    int t1;
+    int t2;
+
+    int p1;
+    int p2;
+
+    int k1;
+    int k2;
 
     void limit()
     {
-        p = C.GetComponent<objectHp>();
+        p = C.GetComponent<ObjDestroy>();
     }
 
     // Use this for initialization
     void Start () {
         //Application.LoadLevelAdditive(V_O_D);
-        C = (GameObject)Resources.Load("Prefabs/BigCube");
+        C = (GameObject)Resources.Load("Prefabs/BigClystal");
         DontDestroyOnLoad(this);
-        DontDestroyOnLoad(canvas);
+        //DontDestroyOnLoad(canvas);
     }
 
     // Update is called once per frame
@@ -41,19 +60,38 @@ public class limitTime : MonoBehaviour {
             if (playTime > 0)
                 playTime -= Time.deltaTime;
 
-            timeText.text = playTime.ToString("#.0");
+            timeText1.text = playTime.ToString("#.0");
+            timeText2.text = playTime.ToString("#.0");
             Invoke("limit", 1f);
             if (playTime < 0)
             {
-                timeText.text = "0.0";
+                timeText1.text = "0.0";
+                timeText2.text = "0.0";
                 playTime = 0;
 
-                Debug.Log(p.result());
-                pp = p.result();
+                p1 = p.po1();
+                p2 = p.po2();
 
-                panel.transform.parent = null;
-                camera.transform.parent = null;
-                //SceneManager.LoadScene(V_O_D);
+                k1 = p.p1_kill();
+                k2 = p.p2_kill();
+
+                t1 = p1 + k1 - k2;
+                t2 = p2 + k2 - k1;
+
+                if (t1 < 0) t1 = 0;
+                if (t2 < 0) t2 = 0;
+
+                if (t1 > t2) textColor = "red";
+                else if (t1 < t2) textColor = "blue";
+
+                pp = p.result();
+                p1Kill = k1.ToString();
+                p2Kill = k2.ToString();
+                p1Point = p1.ToString();
+                p2Point = p2.ToString();
+                p1Total = t1.ToString();
+                p2Total = t2.ToString();
+
                 SceneManager.LoadScene("result");
 
             }
